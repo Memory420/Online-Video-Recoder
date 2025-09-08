bd71b7feccc18f53435a406bffcd87162c518d22
scotium <ihatesspies@gmail.com>
2025-09-08 13:14:50 +0300

    fix: enable scheduling in Application class

--- src/main/java/com/memory/Repositories/VideoRepository.java
+++ src/main/java/com/memory/Repositories/VideoRepository.java
@@ -1,8 +1,16 @@
 package com.memory.Repositories;
 
+import com.memory.Enums.JobStatus;
 import com.memory.Models.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.OffsetDateTime;
+import java.util.List;
+
 public interface VideoRepository extends JpaRepository<Video, Long> {
     Video findByInputPath(String name);
+
+    Video findByChecksum(String checksum);
+
+    List<Video> findVideosByStatusAndExpiresAtBefore(JobStatus status, OffsetDateTime now);
 }
