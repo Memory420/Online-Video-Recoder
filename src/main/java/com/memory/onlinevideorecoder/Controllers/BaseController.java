523ab831ab6a7fbccc1cb74fcb7f33561880f7e8
scotium <ihatesspies@gmail.com>
2025-09-22 14:39:35 +0300

    feat: add video save to database

--- src/main/java/com/memory/onlinevideorecoder/Controllers/BaseController.java
+++ src/main/java/com/memory/onlinevideorecoder/Controllers/BaseController.java
@@ -1,5 +1,13 @@
 package com.memory.onlinevideorecoder.Controllers;
 
+import com.memory.onlinevideorecoder.Config.PropertiesStorage;
+import com.memory.onlinevideorecoder.Models.Video;
+import com.memory.onlinevideorecoder.Models.VideoStatus;
+import com.memory.onlinevideorecoder.Repositories.VideoRepository;
+import com.memory.onlinevideorecoder.Utils.FileChecksum;
+import org.apache.commons.io.FilenameUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +15,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
+
 @Controller
 public class BaseController {
+    private final Logger log = LoggerFactory.getLogger(BaseController.class);
+    private final VideoRepository vr;
+    private final String uploadDir;
+
+
+    public BaseController(VideoRepository vr, PropertiesStorage ps) {
+        this.vr = vr;
+        this.uploadDir = ps.getInputDir();
+    }
 
     @GetMapping("/")
     public String home() {
@@ -21,9 +45,31 @@ public class BaseController {
             @RequestParam("preset") String preset,
             @RequestParam("qp") int qp,
             Model model
-    ) {
+    ) throws IOException, NoSuchAlgorithmException {
+        Path inputDir = Paths.get(System.getProperty("user.dir") + uploadDir);
+        log.debug("inputDir: {}", inputDir);
+        if (!Files.exists(inputDir)) {
+            Files.createDirectories(inputDir);
+        }
+        Path target = inputDir.resolve(file.getOriginalFilename());
+        log.debug("target: {}", target);
+        file.transferTo(target);
+
+
+        Video video = new Video(); // ручное тестирование
+        video.setInputSizeBytes(Files.size(target));
+        video.setInputPath(target);
+        video.setChecksum(FileChecksum.calculateSHA256(target));
+        video.setName(FilenameUtils.getBaseName(target.toString()));
+        video.setCreatedAt(LocalDateTime.now());
+        video.setStatus(VideoStatus.QUEUED);
+        vr.save(video);
+
         String msg = String.format("File: %s<br>Preset: %s<br>QP: %s",
                 file.getOriginalFilename(), preset, qp);
+
+        log.debug(msg);
+
         model.addAttribute("message", msg);
         return "result";
     }
